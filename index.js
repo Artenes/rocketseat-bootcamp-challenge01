3b1bcc350701ee341bf4bba8fd19d09010d34024
Artenes Nogueira <artenesama@gmail.com>
2019-08-24 18:23:37 -0400

    Removes findProjectIndexById method and improve readability

--- index.js
+++ index.js
@@ -9,41 +9,43 @@ server.use(express.json());
 const projects = [];
 let counter = 0;
 
-//finds the index of a project by its id, -1 if none was found
-function findProjectIndexById(id) {
-  for (let index = 0; index < projects.length; index++) {
-    if (projects[index].id == id) {
-      return index;
-    }
-  }
-  return -1;
-}
-
 //global middleware to track the amount of requests made
 server.use((req, res, next) => {
-  console.log(`Number of requests: ${++counter}`);
-  next();
+  counter++;
+
+  console.log(`Number of requests: ${counter}`);
+
+  return next();
 });
 
 //local middleware to validate if project exists
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
-  const index = findProjectIndexById(id);
-  if (index != -1) {
-    req.project = projects[index];
-    req.projectIndex = index;
-    return next();
+
+  const index = projects.findIndex(project => project.id == id);
+
+  if (index == -1) {
+    return res.status(404).send();
   }
-  return res.status(404).send();
+
+  //adds project information in request for easy access in the route
+  req.project = projects[index];
+  req.projectIndex = index;
+
+  return next();
 }
 
 //local middleware to block creating a project with an existing id
 function checkIdExists(req, res, next) {
   const { id } = req.body;
-  const notFound = findProjectIndexById(id) == -1;
-  return notFound
-    ? next()
-    : res.status(400).json({ error: `Id ${id} already in use` });
+
+  const found = projects.find(project => project.id == id) || false;
+
+  if (found) {
+    return res.status(400).json({ error: `Id ${id} already in use` });
+  }
+
+  return next();
 }
 
 //local middleware to check if project data is valid before creating it
@@ -76,11 +78,15 @@ function checkProjectDataIsValid(req, res, next) {
 //creates a project
 server.post("/projects", checkIdExists, checkProjectDataIsValid, (req, res) => {
   const { id, title } = req.body;
-  projects.push({
+
+  const project = {
     id,
     title,
     tasks: []
-  });
+  };
+
+  projects.push(project);
+
   return res.status(201).send();
 });
 
@@ -92,20 +98,25 @@ server.get("/projects", (req, res) => {
 //edit a project
 server.put("/projects/:id", checkProjectExists, (req, res) => {
   const { title } = req.body;
+
   req.project.title = title;
+
   return res.json(req.project);
 });
 
 //delete a project
 server.delete("/projects/:id", checkProjectExists, (req, res) => {
   projects.splice(req.projectIndex, 1);
+
   return res.send();
 });
 
 //create a task
 server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
   const { title } = req.body;
+
   req.project.tasks.push(title);
+
   return res.status(201).send();
 });
 
